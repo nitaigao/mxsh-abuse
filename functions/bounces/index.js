b404b6ece753986187e1f02e8f002866dbd4a5c1
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-12 20:05:01 +0800

    complaints work

--- functions/bounces/index.js
+++ functions/bounces/index.js
@@ -5,7 +5,7 @@ exports.handle = function(e, ctx, cb) {
   recipients = records.map(record => 
     JSON.parse(record.Sns.Message).bounce.bouncedRecipients.map(recipient => 
         recipient.emailAddress.replace('<', '').replace('>', '')))
-  var offenders = [].concat.apply([], recipients)
+  const offenders = [].concat.apply([], recipients)
   offenders.forEach(offender => {
     console.log(`reporting ${offender}`)
     request.post('https://mxsh.io/api/abuse/bounces').form({email: offender})
--- functions/complaints/index.js
+++ functions/complaints/index.js
@@ -3,11 +3,10 @@ const request = require('request')
 exports.handle = function(e, ctx, cb) {
   console.log(e)
   records = e.Records
-  recipients = records.map(record => 
-    console.log(record.Sns.Message)
-    JSON.parse(record.Sns.Message).bounce.bouncedRecipients.map(recipient => 
+  recipients = records.map(record =>
+    JSON.parse(record.Sns.Message).complaint.complainedRecipients.map(recipient => 
         recipient.emailAddress.replace('<', '').replace('>', '')))
-  var offenders = [].concat.apply([], recipients)
+  const offenders = [].concat.apply([], recipients)
   offenders.forEach(offender => {
     console.log(`reporting ${offender}`)
     request.post('https://mxsh.io/api/abuse/complaints').form({email: offender})
